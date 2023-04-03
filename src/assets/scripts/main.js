5c86923384e98477ce156a8ebf35ab5f779bce95
utopia-code <utopiacode.developer@gmail.com>
2023-04-03 16:39:58 +0200

    Gallery Layout, Fonts, content, install and config posthtml-expressions and isotope dependencies

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -6,11 +6,55 @@
 // import 'some-node-module';
 // import SomeModule from 'some-node-module';
 
+
+import Isotope from 'isotope-layout';
+
 /**
  * Write any other JavaScript below
  */
 
+
+// init isotope gallery;
+
 +( function() {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
+	var iso = new Isotope( '.grid', {
+		itemSelector: '.grid-item',
+		percentPosition: true,
+		masonry: {
+			columnWidth: '.grid-sizer',
+			gutter: 10
+		}
+	});
+	
+	iso.arrange({ filter: '.triangle' });
+	
+	let filtersElem = document.querySelectorAll('.filter-button-group .button');
+	
+	filtersElem.forEach(function(filterElem) {
+		filterElem.addEventListener( 'click', function() {
+			var filterValue = this.getAttribute('data-filter');
+			iso.arrange({ filter: filterValue });
+		});
+	});
 } )();
+
+
+// set class in active buttons isotope gallery;
+
++( function() {
+	let buttonGroup = document.querySelectorAll('.button-group .button');
+
+	buttonGroup.forEach(function(button) {
+		button.addEventListener('click', function(e) {
+			document.querySelector('.is-checked').classList.remove('is-checked');
+			button.classList.add('is-checked');
+		});
+	});
+} )();
+
+
+
+
+
+
+
