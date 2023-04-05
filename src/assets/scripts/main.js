01e52deda99a3ebbd65e98e443b7ee72fb493599
utopia-code <utopiacode.developer@gmail.com>
2023-04-05 21:57:23 +0200

    JS: - add arrange isotope when load - accordion (desktop / mobile)

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -30,8 +30,13 @@ var fslightbox = require("fslightbox");
 			category: '[data-category]'
 		}
 	});
+
+	window.addEventListener('load', (event) => {
+		iso.arrange({ filter: '.tab-triangle' });
+		iso.arrange({ sortBy: 'random' });
+	  });
 	
-	iso.arrange({ filter: '.tab-triangle' });
+	
 	
 	let filtersElem = document.querySelectorAll('.filter-button-group .button');
 	
@@ -39,7 +44,6 @@ var fslightbox = require("fslightbox");
 		filterElem.addEventListener( 'click', function() {
 			var filterValue = this.getAttribute('data-filter');
 			iso.arrange({ filter: filterValue });
-			iso.arrange({ sortBy: 'random' });
 		});
 	});
 } )();
@@ -79,6 +83,59 @@ var fslightbox = require("fslightbox");
 
 })();
 
+
++(function () {
+    
+    // accordion
+
+    let containers = document.querySelectorAll('.accordion-panel');
+	let buttons = document.querySelectorAll('.js-accordion-button');
+	let buttons_mobile = document.querySelectorAll('.js-accordion-button-mobile');
+
+	function show_content(elem) {
+		clear_panel();
+		elem.classList.add('active');
+	}
+
+	buttons.forEach(function(button, index){
+		button.addEventListener('click', function() {
+			if (hasClass(button, 'active') != true) {
+				show_content(containers[index])
+				this.classList.add('active');
+			} else {
+				clear_panel();
+			}
+		});
+	});
+
+	buttons_mobile.forEach(function(button_mobile, index){
+		button_mobile.addEventListener('click', function() {
+			if (hasClass(button_mobile, 'active') != true) {
+				show_content(containers[index])
+				this.classList.add('active');
+			} else {
+				clear_panel();
+			}
+		});
+	});
+
+	function clear_panel() {
+		containers.forEach(function(container){
+			container.classList.remove('active');
+		});
+		buttons.forEach(function(button){
+			button.classList.remove('active');
+		});
+		buttons_mobile.forEach(function(button_mobile){
+			button_mobile.classList.remove('active');
+		});
+	}
+
+	function hasClass(element, className) {
+		return (' ' + element.className + ' ').indexOf(' ' + className+ ' ') > -1;
+	}
+
+})();
 
 
 
